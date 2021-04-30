5de68d2dbf8c52f973f4f9757a2ae6ccb018cfee
iderraz <iderraz@hotmail.com>
2021-04-30 14:34:29 +0200

    variables exo-4

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+
+/*exo1*/
+/*
+let prenom = prompt("Quel est ton prénom ?");
+alert("Bonjour " + prenom);
+
+/*exo2*/
+/*
+let age = prompt("Quel est ton age ?");
+alert("Tu as " + age +" ans");
+
+/*exo3*/
+/*
+let nameu =prompt("quel est ton prénom ?");
+let surname= prompt("quel est ton nom de famille");
+
+let entier = (nameu + " " + surname);
+alert("tu t'appelles donc " + " " + entier);
+
+/*----------------------------------------------------*/
+
+/*exo4*/
+
+let prenom2 =prompt("Quel est ton prénom ?");
+let surname2 =prompt("Quel est ton nom de famille ?");
+
+
+let entier2 = (prenom2 + " " + (surname2.toUpperCase()));
+alert( "Bonjour " + entier2);
+
+
+
+
+
+
